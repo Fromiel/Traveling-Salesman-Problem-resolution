8026ba9c107e4992f0d54a3affebafdb5d38ea99
Fromiel <79905976+Fromiel@users.noreply.github.com>
2022-12-13 13:05:30 -0500

    ++ threads

--- src/Main.java
+++ src/Main.java
@@ -14,11 +14,11 @@ public class Main {
 		f.setVisible(true);
 
 		// villes, borne sup, borne inf, coeff d'evaporation en %
-		int nbVilles = 10, nbIterations = 1000000, nbFourmis = 100;
+		int nbVilles = 10, nbIterations = 100000000, nbFourmis = 100, nbThreads = 10;
 
 		int bestLength = -1; // Meilleur solution trouvée
 
-		Problem p = new Problem(nbVilles, 1, 5000, 0.1f, 500, 500);
+		Problem p = new Problem(nbVilles, 1, 5000, 0.09f, 500, 500);
 
 		// ajoute les villes à la fenêtre
 		f.getContentPane().add(new Interface(p.cities, null));
@@ -26,18 +26,9 @@ public class Main {
 		System.out.println("Problem créé");
 
 		// nombre de fourmis
-		for (int i = 0; i < 10; i++) {
-			new Thread(new AntSystem(p, nbFourmis, bestLength, nbIterations)).start();
+		for (int i = 0; i < nbThreads; i++) {
+			new Thread(new AntSystem(p, nbFourmis, bestLength, nbIterations / nbThreads)).start();
 		}
-		// AntSystem sys = new AntSystem(p, nbFourmis);
 
-		System.out.println("Système créé");
-
-		// unités de temps
-		// sys.run(nbIterations);
-
-		System.out.println("Chemins testés");
-
-		System.out.println("Fin du programme");
 	}
 }
